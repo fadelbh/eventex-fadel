1d49cc81b436203cef0b2cee68e4f4af63c5a6ce
HumbertoFadel <fadelbh@hotmail.com>
2013-11-27 22:35:41 -0200

    All your base are belong to us

---
+++ eventex/urls.py
@@ -0,0 +1,13 @@
+    # Examples:
+    # url(r'^$', 'eventex.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^$', 'eventex.core.views.home', name='home'),
+
+    url(r'^admin/', include(admin.site.urls)),
+)
